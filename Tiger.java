9aaf344c93fa5f280b87fa7f2945b9d6f37f8565
AlexRomanofff <romanov23@bigmir.net>
2015-10-30 16:36:50 +0200

    New Tanks

---
+++ Tiger.java
@@ -0,0 +1,14 @@
+public class Tiger extends Tank {
+	
+	private int armor;
+	
+	public Tiger  (ActionField af, BattleField bf) {
+		super(af,bf);
+		armor = 1;
+	}
+	
+	public Tiger (ActionField af, BattleField bf, int x, int y, Direction direction, int armor) {
+		super(af, bf, x, y, direction);
+		this.armor = armor;
+	}
+}
